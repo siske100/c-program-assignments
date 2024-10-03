70a29048372d222a90752ee3df7303ca64cd5789
siske100 <166230659+siske100@users.noreply.github.com>
2024-10-03 09:16:55 +0300

    Add files via upload

---
+++ CI .c
@@ -0,0 +1,21 @@
+//
+/*
+Program to find cmpound interest 
+Authour: Mayoye Joel Kesis
+Date: 25/09/2024
+*/
+
+// preprocessor directive 
+#include<stdio.h>
+int main()
+{
+float P , A , t , r , n ;
+ P = 34000 
+ r = 1.5
+ t = 10 
+ n = 5 
+ A = P(1+r/n)^nt
+ scanf("%f", Amount, A);
+ printf("Amount, %f", A);
+ return(0);
+ }
---
+++ Joel.c
@@ -0,0 +1,13 @@
+//
+/* 
+program print hello world
+Authour: Mayoye Joel 
+Date: 14/10/2024
+*/
+//processor directive 
+#include <stdio.h> // scanf(), printf()
+int main(){
+printf("hello world \n");
+printf("I am mejja siske \n");
+return 0;
+}
